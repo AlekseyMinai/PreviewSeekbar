430a423f7ca6a154ed87e442ebd35fb37d97e2f2
AlekseyMinai <aleksejzuke@gmail.com>
2022-03-08 23:08:04 +0300

    Исправил VideoPlayerState

--- app/src/main/java/com/alexey/minay/videoplayer/VideoPlayerState.kt
+++ app/src/main/java/com/alexey/minay/videoplayer/VideoPlayerState.kt
@@ -6,7 +6,7 @@ data class VideoPlayerState(
 ) {
     companion object {
         fun default() = VideoPlayerState(
-            url = "https://aflet.ispringlearn.ru/proxy/learn-cnode-0/content/200123-9FccD-RoZXp-ZXMAN/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdSI6ImlzbHJ1LTIwMDEyMyIsInV1IjoiNDMzMGM0MTItNjZjNi0xMWViLWI3NTctMGUzM2E1ZmQ0NDBlIiwiY2siOiIyMDAxMjMtOUZjY0QtUm9aWHAtWlhNQU4iLCJhbSI6MiwiZXQiOjE2NDcyNzM4MzZ9.J2goU8A4EKqa3Z4s0rWuC0Wx-tbdiFBYEWScmidCaXk/video.mp4",
+            url = "",
             isPlaying = true
         )
     }
