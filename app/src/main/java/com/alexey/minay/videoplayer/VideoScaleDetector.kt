f96b2e7d6ee7ca19e9b4c16bbaedfc286a8858bd
AlekseyMinai <aleksejzuke@gmail.com>
2022-03-13 13:28:15 +0300

    Доработал scale

--- app/src/main/java/com/alexey/minay/videoplayer/VideoScaleDetector.kt
+++ app/src/main/java/com/alexey/minay/videoplayer/VideoScaleDetector.kt
@@ -3,7 +3,6 @@ package com.alexey.minay.videoplayer
 import android.animation.ValueAnimator
 import android.view.ScaleGestureDetector
 import android.view.View
-import androidx.core.animation.addListener
 import com.alexey.minay.videoplayer.utils.isPortrait
 
 class VideoScaleDetector(
@@ -13,8 +12,8 @@ class VideoScaleDetector(
 ) : ScaleGestureDetector.OnScaleGestureListener {
 
     private var mTotalScale = DEFAULT_SCALE
-    private var mIsStarted = false
-    private var mValueAnimator: ValueAnimator? = null
+    private var mFrameValueAnimator: ValueAnimator? = null
+    private var mPlayerValueAnimator: ValueAnimator? = null
 
     override fun onScale(p0: ScaleGestureDetector): Boolean {
         val playerHeight = player.height
@@ -22,25 +21,30 @@ class VideoScaleDetector(
         val screenHeight = root.height
         val screenWidth = root.width
 
-        val newScale = mTotalScale * p0.scaleFactor
+        var newScale = mTotalScale * p0.scaleFactor
 
         if (player.context.isPortrait()) {
-            if (
-                playerHeight < playerWidth ||
-                playerHeight >= screenHeight ||
-                playerHeight * newScale > screenHeight
-            ) {
+            if (playerHeight < playerWidth) {
                 return false
             }
+
+            if (playerHeight * newScale >= screenHeight) {
+                newScale = screenHeight.toFloat() / playerHeight
+            }
+
         } else {
-            if (
-                playerWidth < playerHeight ||
-                playerWidth >= screenWidth ||
-                playerWidth * newScale > screenWidth
-            ) return false
+            if (playerWidth < playerHeight) {
+                return false
+            }
+
+            if (playerWidth * newScale >= screenWidth) {
+                newScale = screenWidth.toFloat() / playerWidth
+            }
         }
 
-        if (newScale < 1) return false
+        if (newScale < 1) {
+            newScale = DEFAULT_SCALE
+        }
 
         mTotalScale = newScale
         player.scaleX = mTotalScale
@@ -49,7 +53,7 @@ class VideoScaleDetector(
     }
 
     override fun onScaleBegin(p0: ScaleGestureDetector): Boolean {
-        animate(0f, 0.4f)
+        animateFrame(0f, 0.4f)
 
         player.pivotX = root.width.toFloat() / 2
         player.pivotY = root.height.toFloat() / 2
@@ -58,22 +62,48 @@ class VideoScaleDetector(
 
     override fun onScaleEnd(p0: ScaleGestureDetector) {
         when (mTotalScale) {
-            DEFAULT_SCALE -> animate(0.4f, 0f)
-            else -> animate(0.4f, 0.6f, 0.4f, 0f)
+            DEFAULT_SCALE -> animateFrame(0.4f, 0f)
+            else -> animateFrame(0.4f, 0.6f, 0.4f, 0f)
+        }
+        anim()
+    }
+
+    private fun animateFrame(vararg values: Float) {
+        mFrameValueAnimator?.cancel()
+        mFrameValueAnimator = ValueAnimator.ofFloat(*values)
+        mFrameValueAnimator?.addUpdateListener { valueAnimation ->
+            gestureInterceptor.alpha = valueAnimation.animatedValue as Float
+        }
+        mFrameValueAnimator?.start()
+    }
+
+    private fun anim() {
+        val playerHeight = player.height
+        val playerWidth = player.width
+        val screenHeight = root.height
+        val screenWidth = root.width
+        if (player.context.isPortrait()) {
+
+        } else {
+            val maxScale = screenWidth.toFloat() / playerWidth
+            if (maxScale - mTotalScale < (maxScale - DEFAULT_SCALE) / 2) {
+                animatePlayer(mTotalScale, maxScale)
+            } else {
+                animatePlayer(mTotalScale, DEFAULT_SCALE)
+            }
         }
     }
 
-    private fun animate(vararg values: Float) {
-        mValueAnimator?.cancel()
-        mValueAnimator = ValueAnimator.ofFloat(*values)
-        mValueAnimator?.addUpdateListener { valueAnimation ->
-            gestureInterceptor.alpha = valueAnimation.animatedValue as Float
+    private fun animatePlayer(vararg values: Float) {
+        mPlayerValueAnimator?.cancel()
+        mPlayerValueAnimator = ValueAnimator.ofFloat(*values)
+        mPlayerValueAnimator?.duration = 100
+        mPlayerValueAnimator?.addUpdateListener { valueAnimation ->
+            mTotalScale = valueAnimation.animatedValue as Float
+            player.scaleX = mTotalScale
+            player.scaleY = mTotalScale
         }
-        mValueAnimator?.addListener(onEnd = {
-            mIsStarted = false
-        })
-        mValueAnimator?.start()
-        mIsStarted = true
+        mPlayerValueAnimator?.start()
     }
 
     companion object {
